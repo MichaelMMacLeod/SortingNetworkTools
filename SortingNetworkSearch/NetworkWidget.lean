4ecf9c2b510219f588b0873be6c25dd7b19235c9
Michael MacLeod <michaelmmacleod@gmail.com>
2025-08-13 06:28:17 -0700

    Get more generic cata working kind of

--- SortingNetworkSearch/NetworkWidget.lean
+++ SortingNetworkSearch/NetworkWidget.lean
@@ -5,6 +5,189 @@ import Std.Data.HashMap
 
 open Lean Widget
 
+
+inductive ListF (α : Type) (β : Type)
+  | nil : ListF α β
+  | cons : α → β → ListF α β
+
+@[specialize, inline]
+def List.project : List α → ListF α (List α)
+  | .nil => .nil
+  | .cons a b => .cons a b
+
+def ListF.embed : ListF α (List α) → List α
+  | .nil => .nil
+  | .cons a b => .cons a b
+
+def ListF.fmap (l : ListF α β) (f : β → χ) : ListF α χ :=
+  match l with
+  | .nil => .nil
+  | .cons a b => .cons a (f b)
+
+-- def List.cata (l : List α) (f : ListF α β → β) : β :=
+--   let rec loop1
+--       (stack : Array (ListF α (β ⊕ Unit)))
+--       (l : List α)
+--       : β :=
+--     match List.project l with
+--     | .nil =>
+--       let rec loop2
+--           (stack : Array (ListF α (β ⊕ Unit)))
+--           (b : β)
+--           : β :=
+--         if 0 < stack.size then
+--           let x :=
+--     | .cons a b =>
+--       let stack := stack.push <| .cons a <| Sum.inr ()
+--       loop1 stack b
+--   sorry
+
+inductive cataState Seed α where
+  | expand : Nat → Seed → cataState Seed α
+  | collapse : Nat → α → cataState Seed α
+
+#check Array.mapM
+
+#check StateT.bind
+
+    -- (mapM : {β χ : Type} → {m : Type → Type} → [Monad m] → Frame β → (β → m χ) → m (Frame χ))
+
+def myListFmap (state : State) (lst : ListF α β) (f : State → β → State × χ) : State × ListF α χ :=
+  match lst with
+  | .nil => (state, .nil)
+  | .cons a b =>
+    let (state, c) := f state b
+    (state, .cons a c)
+
+abbrev IDK1 := (s t u : Type) → (f : Type → Type) → s → f t → (s → t → s × u) → s × f u
+abbrev IDK2 := {s : Type → Type} → (t u : Type) → [Monad s] → (f : Type → Type) → s (f t) → (s t → s u) → s (f u)
+
+@[specialize]
+partial def cata
+    [Inhabited β]
+    {Frame : Type → Type}
+    {α}
+    (a : α)
+    (project : α → Frame α)
+    (f : Frame β → β)
+    (fmapWithState : {State : Type} → {t u : Type} → State → Frame t → (State → t → State × u) → State × Frame u)
+    : β :=
+  let rec @[specialize] loop
+      (vals : Array (Option β))
+      (stack : Array (cataState α (Frame Nat)))
+      : β :=
+    if h : 0 < stack.size then
+      let item := stack[stack.size - 1]
+      let stack := stack.pop
+      match item with
+      | .expand valIdx seed =>
+        let node := project seed
+        let seeds := #[]
+        let ((vals, seeds), node) := fmapWithState (vals, seeds) node fun (vals, seeds) seed =>
+          let vals := vals.push none
+          let idx := vals.size - 1
+          let seeds := seeds.push <| .expand idx seed
+          ((vals, seeds), idx)
+        let stack := stack.push <| .collapse valIdx node
+        let stack := stack.append seeds
+        loop vals stack
+      | .collapse valIdx node =>
+        let (vals, node) := fmapWithState vals node fun vals k =>
+          let (vals, node) := vals.swapRemove! k none
+          (vals, node.get!)
+        let vals := vals.set! valIdx (f node)
+        loop vals stack
+    else
+      (vals[0]!).get!
+  loop #[none] #[.expand 0 a]
+
+def lst := List.range 100000
+set_option trace.profiler.output "./ffperf"
+set_option trace.profiler true
+#eval lst.foldr (· + ·) 0
+def v := cata (β := Nat) lst List.project
+  (fun lst =>
+    match lst with
+    | .nil => 0
+    | .cons a b => a + b)
+  (fun state xs f =>
+  match xs with
+  | .nil => (state, .nil)
+  | .cons a b =>
+    let (state, c) := f state b
+    (state, .cons a c))
+#eval! v
+
+-- def expandAndCollapse
+--     [Inhabited α]
+--     {Frame : Type → Type}
+--     {Seed}
+--     (seed : Seed)
+--     (project : Seed → Frame Seed)
+--     (embed : Frame α → α)
+--     (fmap : {State : Type} → {t u : Type} → State × Frame t → (State → t → State × u) → State × Frame u)
+--     : α :=
+--   let rec loop
+--       (vals : Array (Option α))
+--       (stack : Array (ExpandAndCollapseState Seed (Frame Nat)))
+--       : α :=
+--     if h : 0 < stack.size then
+--       let item := stack[stack.size - 1]
+--       let stack := stack.pop
+--       match item with
+--       | .expand valIdx seed =>
+--         let node := project seed
+--         let seeds := #[]
+--         let ((vals, seeds), node) := fmap ((vals, seeds), node) fun (vals, seeds) seed =>
+--           let vals := vals.push none
+--           let idx := vals.size - 1
+--           let seeds := seeds.push <| .expand idx seed
+--           ((vals, seeds), idx)
+--         let stack := stack.push <| .collapse valIdx node
+--         let stack := stack.append seeds
+--         loop vals stack
+--       | .collapse valIdx node =>
+--         let (vals, node) := fmap (vals, node) fun vals k =>
+--           let (vals, node) := vals.swapRemove! k none
+--           (vals, node.get!)
+--         let vals := vals.set! valIdx (embed node)
+--         loop vals stack
+--     else
+--       vals[0]!.get!
+--   loop #[none] #[.expand 0 seed]
+
+-- #eval! expandAndCollapse (α := Nat) [1,2,3] List.project
+--   (fun lst =>
+--     match lst with
+--     | .nil => 0
+--     | .cons a b => a + b)
+--   (fun (state, xs) f =>
+--   match xs with
+--   | .nil => (state, .nil)
+--   | .cons a b =>
+--     let (state, c) := f state b
+--     (state, .cons a c))
+
+/-
+-- cata f = c where c = f . fmap c . project
+-- cata f x = f (fmap (cata f) (project x))
+
+cata f [1,2,3]
+f (fmap (cata f) 1 :: [2,3])
+f (1 :: (cata f [2,3]))
+f (1 :: (fmap (cata f) 2 :: [3]))
+f (1 :: (2 :: (cata f [3])))
+f (1 ::)
+
+stack = [[1,2,3]]
+
+stack = [1, [2, 3]]
+-/
+
+
+
+
+
 namespace SVG
 
 abbrev Tags := Std.HashMap String String
@@ -13,41 +196,16 @@ abbrev Tags := Std.HashMap String String
 structure Node where
   name : String
   tags : Tags
-  children : Array Node
+  children : List Node
   deriving Inhabited, Repr
 
 mutual
-@[grind]
 def Node.size : Node → Nat
   | { children, .. } => 1 + Node.sizeAux children
-@[grind]
-def Node.sizeAux (xs : Array Node) : Nat :=
-  xs.map (·.size) |>.sum
+def Node.sizeAux : List Node → Nat
+  | [] => 0
+  | c :: cs => c.size + (Node.sizeAux cs)
 end
-
-mutual
-@[grind]
-theorem Node.size_lt (n : Node) {i : Nat} {h} {c : Node}
-    : c = n.children[i] → c.size < n.size := by
-  sorry
--- theorem Node.sizeAux_lt (xs : Array Node) :
---    c = xs[i] → c.size < xs.map (·.size) |>.sum := by
-end
-
--- theorem Node.size_children_lt (n : Node) {i : Nat} {h} {c : Node}
---     : c = n.children[i] → c.size < n.size := by
---   intro c_eq
---   induction h : n.size with
---   | zero => grind
---   | succ n' ih =>
---     unfold size
---     split
---     rename_i children tags name
---     unfold sizeAux
---     grind
---     -- simp [*]
-
---     -- grind
 
 def Tags.toString (tags : Std.HashMap String String) : String := Id.run do
   let mut result := ""
@@ -55,6 +213,25 @@ def Tags.toString (tags : Std.HashMap String String) : String := Id.run do
     result := result ++ s!"{tag}=\"{value}\" "
   result := result.dropRight 1
   result
+
+structure Tree (α : Type) where
+  label : α
+  children : List (Tree α)
+  deriving ToExpr
+
+structure TreeF (α : Type) (β : Type) where
+  label : α
+  children : List β
+  deriving ToExpr
+
+def Tree.project (t : Tree α) : TreeF α (Tree α) := { t with }
+def TreeF.embed (t : TreeF α (Tree α)) : Tree α := { t with }
+
+def t1 : Tree Nat := Tree.mk 0 [Tree.mk 1 [], Tree.mk 2 [], Tree.mk 3 []]
+#eval t1.project.embed
+
+def Tree.cata (t : Tree α) (f : TreeF α β → β) : β :=
+  sorry
 
 @[grind]
 structure NodeF α where
@@ -68,34 +245,6 @@ def NodeF.size {α} (_n : NodeF α) := 0
 
 abbrev Stack α := Array ((Nat × Node) ⊕ (Nat × NodeF α))
 abbrev Acc α := Array (Array α)
-
-def Node.cata' [Inhabited α] (n : Node) (f : NodeF α → α) : α := Id.run do
-  let mut stack : Array ((Nat × Node) ⊕ (Nat × NodeF α)) := #[Sum.inl (0, n)]
-  let mut acc : Array (Array α) := #[.emptyWithCapacity n.children.size]
-  let mut isFirstIteration := true
-  while h : stack.size > 1 ∨ (stack.size = 1 ∧ isFirstIteration) do
-    isFirstIteration := false
-    let n := stack[stack.size - 1]
-    stack := stack.pop
-    match n with
-    | Sum.inl (i, n) =>
-      stack := stack.push (Sum.inr (i, { n with children := #[] }))
-      let mut children := n.children
-      acc := acc.push (.emptyWithCapacity children.size)
-      let nIndex := stack.size - 1
-      while h : children.size > 0 do
-        acc := acc.push #[]
-        stack := stack.push (Sum.inl (nIndex, children[children.size - 1]))
-        children := children.pop
-    | Sum.inr (i, n) =>
-      let as := acc[acc.size - 1]!
-      acc := acc.pop
-      let a := f { n with children := as }
-      acc := acc.set! i (acc[i]!.push a)
-  match stack[0]? with
-  | none | some (Sum.inl _) => panic! ""
-  | some (Sum.inr (i, n)) => f { n with children := acc[0]?.get! }
-
 
 def Node.cata (n : Node) (f : NodeF α → α) : α :=
   let rec loop
@@ -134,9 +283,35 @@ def Node.cata (n : Node) (f : NodeF α → α) : α :=
   termination_by orig_node.size - fuel
   decreasing_by
     simp_wf
-    
     grind
   loop #[.emptyWithCapacity n.children.size] ⟨#[Sum.inl (0, n)], by grind⟩ 0
+
+def Node.cata' [Inhabited α] (n : Node) (f : NodeF α → α) : α := Id.run do
+  let mut stack : Array ((Nat × Node) ⊕ (Nat × NodeF α)) := #[Sum.inl (0, n)]
+  let mut acc : Array (Array α) := #[.emptyWithCapacity n.children.size]
+  let mut isFirstIteration := true
+  while h : stack.size > 1 ∨ (stack.size = 1 ∧ isFirstIteration) do
+    isFirstIteration := false
+    let n := stack[stack.size - 1]
+    stack := stack.pop
+    match n with
+    | Sum.inl (i, n) =>
+      stack := stack.push (Sum.inr (i, { n with children := #[] }))
+      let mut children := n.children
+      acc := acc.push (.emptyWithCapacity children.size)
+      let nIndex := stack.size - 1
+      while h : children.size > 0 do
+        acc := acc.push #[]
+        stack := stack.push (Sum.inl (nIndex, children[children.size - 1]))
+        children := children.pop
+    | Sum.inr (i, n) =>
+      let as := acc[acc.size - 1]!
+      acc := acc.pop
+      let a := f { n with children := as }
+      acc := acc.set! i (acc[i]!.push a)
+  match stack[0]? with
+  | none | some (Sum.inl _) => panic! ""
+  | some (Sum.inr (i, n)) => f { n with children := acc[0]?.get! }
 
 
   -- let (stack, acc) :=
