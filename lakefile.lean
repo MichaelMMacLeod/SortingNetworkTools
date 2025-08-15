ab4fc13e2b4e9270e4bb354f44663023f7c15eb6
Michael MacLeod <michaelmmacleod@gmail.com>
2025-08-15 00:21:37 -0700

    Add Cli dependency, update toolchain version

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,8 @@ open System Lake DSL
 package SortingNetworkSearch where
   version := v!"0.1.0"
 
+require "leanprover" / "Cli" @ "git#cacb481a1eaa4d7d4530a27b606c60923da21caf"
+
 lean_lib SortingNetworkSearch where
 
 @[default_target]
