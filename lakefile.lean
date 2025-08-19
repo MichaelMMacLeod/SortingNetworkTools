f42c8c3d4c6b9e87b9e11f45a456f36be185f6d1
Michael MacLeod <michaelmmacleod@gmail.com>
2025-08-19 14:35:52 -0700

    Remove unused dependency

--- lakefile.lean
+++ lakefile.lean
@@ -5,9 +5,7 @@ open System Lake DSL
 package SortingNetworkTools where
   version := v!"0.1.0"
 
-require "leanprover" / "Cli" @ "git#cacb481a1eaa4d7d4530a27b606c60923da21caf"
-
-lean_lib SortingNetworkTools where
+lean_lib SortingNetworkTools
 
 @[default_target]
 lean_exe snt where
